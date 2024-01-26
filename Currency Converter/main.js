7482b22117121da3d37d4a43fc72dc6852617114
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2024-01-26 20:48:39 +0530

    changes

--- Currency Converter/main.js
+++ Currency Converter/main.js
@@ -3,22 +3,23 @@ const populate = async (value, currency) => {
     url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_6l55ck7TAi9u0CP6rdkTsQggGrV4ZVq91vsP0TLK&base_currency=" + currency;
     let response = await fetch(url)
     let rJson = await response.json()
-    for (let key in Object.keys(rJson["data"])){
-        myStr += `<tr>
-                        <td> ${key}</td>
-                        <td>${rJson["data"][key]["code"]}</td>
-                        <td>${rJson["data"][key]["value"]* value}</td>
-        </tr>
-    `
-    }
-    const tablebody = document.querySelector("tbody");
-    tablebody.innerHTML = myStr;
-}
+    document.querySelector(".output").style.display = "block"
 
-const btn = document.querySelector('btn');
+    for (let key of Object.keys(rJson["data"])) {
+        myStr += ` <tr>
+                        <td>${key}</td>
+                        <td>${rJson["data"][key]["code"]}</td>
+                        <td>${Math.round(rJson["data"][key]["value"] * value)}</td>
+                    </tr> 
+                `
+    }
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myStr;
+
+}
+const btn = document.querySelector(".btn")
 btn.addEventListener("click", (e) => {
     e.preventDefault()
-    console.log("button is clicked");
     const value = parseInt(document.querySelector("input[name='quantity']").value);
     const currency = document.querySelector("select[name='currency']").value
     populate(value, currency)
