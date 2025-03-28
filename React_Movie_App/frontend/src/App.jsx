54b9b2e013d864fa631d4ad6c98809f422e7abf7
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-03-28 07:00:22 +0530

    changes

--- React_Movie_App/frontend/src/App.jsx
+++ React_Movie_App/frontend/src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// This is the main file of the frontend
+// This is the file where the react app is rendered
 import './App.css'
-
+// the below is the App component
+// it is a functional component
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    // only one parent element is allowed in the return statement
+    // the below is the parent element
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
