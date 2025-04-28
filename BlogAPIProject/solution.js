9ff7df1c0c965a264749362b6c0940ac262f0ff6
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-04-28 07:41:44 +0530

    changes

--- BlogAPIProject/solution.js
+++ BlogAPIProject/solution.js
@@ -3,7 +3,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 // In-memory data store
 let posts = [
